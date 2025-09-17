1e9dc951a1699897be8acb2cc325e73b54724c89
Isaac Stallan <Isaac.Stallan@gmail.com>
2025-09-17 18:41:46 +1000

    card system

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const usersRoutes = require('./routes/users');
+const Card = require('./models/Card');
 
 const app = express();
 
