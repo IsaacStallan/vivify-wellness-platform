28e9f0d2199951de5cd181de4f69559945ed82a7
Isaac Stallan <Isaac.Stallan@gmail.com>
2025-09-26 14:37:06 +1000

    push

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,13 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: [
+    'https://vivifyeducation.netlify.app',  // Make sure this exact URL is here
     'http://localhost:3000',
-    'http://localhost:5000', 
-    'https://vivifyeducation.netlify.app'
+    'http://localhost:5000'
   ],
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 app.use(express.static('../frontend'));
